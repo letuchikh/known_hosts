b833a8379fd9c1123ab92d6783deb24d3acce613
Vladimir <letuchikh@yandex.ru>
2024-10-13 10:11:46 +0300

    hello

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello-new-branch");
-        System.out.println("Bye-new-branch");
+        System.out.println("Hello-new-branch2");
+        System.out.println("Bye-new-branch2");
     }
 }
